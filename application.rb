e4fa344c245e1dbfbc06894c8f94a023cf14c295
Luke Fender <lfender6445@gmail.com>
2016-02-17 13:32:19 -0500

    call stack example

--- application.rb
+++ application.rb
@@ -1,20 +1,78 @@
 require 'sinatra'
+require 'pry'
+
+module ApplicationHelper
+  MAX_STACK = 100
+  def foo
+    1000.times { puts 'foo' }
+  end
+
+  def bar
+    10000.times { puts 'bar' }
+  end
+
+  def baz
+    100000.times { puts 'baz' }
+  end
+
+  def recursion(some_arg = nil)
+    some_undefined_method_0
+  end
+
+  # create stack or sequence of alphabet methods
+  # that each call the next letter if one exists
+  alphabet = ('a'..'z').to_a
+  alphabet.each_with_index do |letter, index|
+    define_method(letter.to_sym) do
+      unless letter == alphabet.last
+        next_letter = alphabet[index + 1]
+        self.send((next_letter.to_sym)) if next_letter
+        sleep(0.1)
+        return next_letter
+      end
+    end
+  end
+
+  def stack
+    a()
+  end
+
+
+  def method_missing(method_sym, *arguments, &block)
+    # the first argument is a Symbol, so you need to_s it if you want to pattern match
+    if method_sym.to_s =~ /^stack_(.*)$/
+      suffix = (method_sym.to_s.split('_').last.to_i + 1)
+      return false if suffix == MAX_STACK
+      self.send "stack_#{suffix}".to_sym
+    end
+  end
+end
 
 class Sinatra::Application
   use Rack::MiniProfiler
-  module ApplicationHelper
-    def foo
-      puts 'foo'
-    end
-    def bar
-      puts 'bar'
-    end
-  end
   helpers ApplicationHelper
 
   get '/' do
+    # flamegraph branches here
+
+    # ~ 0.79% of a sampling
     foo
+
+    # bar ~8% of a sampling
     bar
+
+    # baz ~ 78% of a sampling
+    baz
+    'hello world'
+  end
+
+  get '/recursion' do
+    recursion
+    'hello world'
+  end
+
+  get '/stack' do
+    stack
     'hello world'
   end
 end
